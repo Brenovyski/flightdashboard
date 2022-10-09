f043819f21c549110096dac40f06d4b6ba8652fa
Johan Kwok <johansukwok@gmail.com>
2022-10-09 01:10:43 -0300

    add test companhia aerea

--- flightdashboard/sys_voos/tests.py
+++ flightdashboard/sys_voos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from sys_voos.models import CompanhiaAerea, Voo, Partida, Chegada
+
+class CompanhiaAereaTest(TestCase):
+  @classmethod
+  def setUpTestData(cls):
+      CompanhiaAerea.objects.create(nome='Gol',codigo='GO')
+  def test_criacao_id(self):
+      companhia_1 = CompanhiaAerea.objects.get(nome='Gol')
+      self.assertEqual(companhia_1.id, 1)
+  def test_update_codigo(self):
+      companhia_1 = CompanhiaAerea.objects.get(nome='Gol')
+      companhia_1.codigo = 'GL'
+      companhia_1.save()
+      self.assertEqual(companhia_1.codigo, 'GL')
