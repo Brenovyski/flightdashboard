cadda745c9bf9596fc74a830f1f55d186648bb42
Breno Suguru <suguru.ben@usp.br>
2022-10-16 03:05:19 -0300

    Created basic login system.

--- flightdashboard/flightdashboard/urls.py
+++ flightdashboard/flightdashboard/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('sys_voos.urls')),
     path('admin/', admin.site.urls),
-    path('sys_voos/', include('sys_voos.urls')),
+    path("accounts/", include("django.contrib.auth.urls")), 
     
 ]
