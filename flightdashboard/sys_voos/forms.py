fc41440d5bba9f5d5c4c8afaa678b2075b4975f4
Breno Suguru <suguru.ben@usp.br>
2022-11-18 17:11:37 -0300

    added filter button

--- flightdashboard/sys_voos/forms.py
+++ flightdashboard/sys_voos/forms.py
@@ -2,3 +2,10 @@ from django import forms
 
 class CodigoForm(forms.Form):
     codigo = forms.CharField(label='Código de voo', max_length=10)
+
+class DataForm(forms.Form):
+    data_inicio = forms.DateField(label='Data de início do relatório')
+    data_fim = forms.DateField(label='Data de fim do relatório')
+
+class StatusForm(forms.Form):
+    status = forms.CharField(label='Status')
